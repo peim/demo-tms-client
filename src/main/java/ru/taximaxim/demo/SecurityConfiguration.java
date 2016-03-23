60870d1e15054467ecf75892d5e4ea2f1c8bd4f2
Петров Иван <petrov_im@taximaxim.local>
2016-03-23 08:28:03 +0300

    only one session for user

--- src/main/java/ru/taximaxim/demo/SecurityConfiguration.java
+++ src/main/java/ru/taximaxim/demo/SecurityConfiguration.java
@@ -1,11 +1,16 @@
 package ru.taximaxim.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 import ru.taximaxim.demo.security.CustomAuthenticationProvider;
 import ru.taximaxim.demo.security.CustomLogoutSuccessHandler;
@@ -24,6 +29,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder authentication) throws Exception {
         authentication.authenticationProvider(authenticationProvider);
     }
+    
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        SessionRegistry sessionRegistry = new SessionRegistryImpl();
+        return sessionRegistry;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -39,7 +55,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .logoutSuccessUrl("/home").invalidateHttpSession(true)
                     .deleteCookies("JSESSIONID").permitAll();
 
-        http.sessionManagement().maximumSessions(1);
+        http.sessionManagement()
+            .maximumSessions(1)
+            .expiredUrl("/expired")
+            .maxSessionsPreventsLogin(true)
+            .sessionRegistry(sessionRegistry());
         
         /* http.requiresChannel().anyRequest().requiresSecure();
         
