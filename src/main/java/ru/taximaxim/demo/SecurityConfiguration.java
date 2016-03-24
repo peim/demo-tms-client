479bb82278e76749d33c0e02eade86ceca53db52
Петров Иван <petrov_im@taximaxim.local>
2016-03-24 08:02:30 +0300

    tms-client-lib

--- src/main/java/ru/taximaxim/demo/SecurityConfiguration.java
+++ src/main/java/ru/taximaxim/demo/SecurityConfiguration.java
@@ -1,11 +1,16 @@
 package ru.taximaxim.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 import ru.taximaxim.demo.security.CustomAuthenticationProvider;
 import ru.taximaxim.demo.security.CustomLogoutSuccessHandler;
@@ -16,36 +21,44 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomAuthenticationProvider authenticationProvider;
-    
+
     @Autowired
     private CustomLogoutSuccessHandler logoutSuccessHandler;
-    
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder authentication) throws Exception {
         authentication.authenticationProvider(authenticationProvider);
     }
 
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        SessionRegistry sessionRegistry = new SessionRegistryImpl();
+        return sessionRegistry;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login").permitAll()
-                .and()
-                .logout().logoutSuccessHandler(logoutSuccessHandler).logoutUrl("/logout")
-                    .logoutSuccessUrl("/home").invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID").permitAll();
+        .authorizeRequests()
+        .antMatchers("/", "/home").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login").permitAll()
+        .and()
+        .logout().logoutSuccessHandler(logoutSuccessHandler).logoutUrl("/logout")
+        .logoutSuccessUrl("/home").invalidateHttpSession(true)
+        .deleteCookies("JSESSIONID").permitAll();
 
-        http.sessionManagement().maximumSessions(1);
-        
-        /* http.requiresChannel().anyRequest().requiresSecure();
-        
-        http
-            .portMapper()
-                .http(8080).mapsTo(8443);
-        */
+        http.sessionManagement()
+            .maximumSessions(1)
+            .expiredUrl("/expired")
+            .maxSessionsPreventsLogin(true)
+            .sessionRegistry(sessionRegistry());
     }
 }
